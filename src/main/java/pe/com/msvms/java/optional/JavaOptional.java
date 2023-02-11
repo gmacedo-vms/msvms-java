d1f157c9767cbbabc6c046015221a60ee2acceaa
gmacedo-vms <g.macedochavarria@gmail.com>
2023-02-11 00:42:01 -0500

    StackOverFlow-Java Optional: ofNullable

--- src/main/java/pe/com/msvms/java/optional/JavaOptional.java
+++ src/main/java/pe/com/msvms/java/optional/JavaOptional.java
@@ -1,2 +1,13 @@
-package pe.com.msvms.java.optional;public class JavaOptional {
+package pe.com.msvms.java.optional;
+
+import java.util.Optional;
+
+public class JavaOptional {
+
+    public static void main(String[] args) {
+
+        String value = "null";
+        System.out.println(Optional.ofNullable(value).isPresent() ? "a" : "b");
+
+    }
 }
