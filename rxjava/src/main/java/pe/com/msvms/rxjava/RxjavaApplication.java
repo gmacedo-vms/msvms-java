7314b919d7fc27eb35a5341b8235d468474f62c1
gmacedo-vms <g.macedochavarria@gmail.com>
2023-02-11 01:02:08 -0500

    VMSoftware-Java Projects: RxJava and Spring Webflux

---
+++ rxjava/src/main/java/pe/com/msvms/rxjava/RxjavaApplication.java
@@ -0,0 +1,13 @@
+package pe.com.msvms.rxjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RxjavaApplication.class, args);
+    }
+
+}
